3662858af0d46692c111665dde625a24ae4d32e8
miguelangel <miguelangel.ortizpenaranda@gmail.com>
2026-06-14 12:42:33 +0200

    more documentation

--- entornos_complejos/src/eml_complex_environments/util/mountain_car_reward_env.py
+++ entornos_complejos/src/eml_complex_environments/util/mountain_car_reward_env.py
@@ -1,6 +1,12 @@
 import gymnasium as gym
 
 class MountainCarRewardEnvironment(gym.Wrapper):
+    """
+    Implementa una clase que hereda de Wrapper, que nos permite modificar el método
+    step para alterar la recompensa que se otorga, de forma que no solo tenga en cuenta
+    el descuento por tiempo, sino la posición con respecto al objetivo, así como la velocidad
+    del coche.
+    """
     def __init__(self, env):
         super().__init__(env)
 
@@ -9,7 +15,8 @@ class MountainCarRewardEnvironment(gym.Wrapper):
 
         position = next_state[0]
         speed = next_state[1]
-
+        # Modificamos la recompensa para tener en cuenta el descuento por tiempo, pero también la posición
+        # con respecto al objetivo, y la velocidad
         shaped_reward = -1.0 + abs(position + 0.5) + (speed ** 2) * 10.0
 
         return next_state, shaped_reward, terminated, truncated, info
